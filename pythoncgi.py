0937348bafe8272a8f53d567b46b39c208b008d8
pakpoom <s6201012620210@email.kmutnb.ac.th>
2021-01-18 15:41:01 +0700

    can sort temp but day,time error

--- pythoncgi.py
+++ pythoncgi.py
@@ -2,13 +2,12 @@
 # Importing the 'cgi' module 
 import cgi 
 
-
 print("Content-type: text/html\r\n\r\n") 
 print("<html><body>") 
 print("<h1> Hello Program! </h1>") 
 # Using the inbuilt methods 
 
-form = cgi.FieldStorage() 
+form = cgi.FieldStorage()
 if form.getvalue("name"): 
 	name = form.getvalue("name") 
 	print("<h1>Hello" +name+"! Thanks for using my script!</h1><br>") 
@@ -16,13 +15,38 @@ if form.getvalue("happy"):
 	print("<p> Yayy! I'm happy too! </p>") 
 if form.getvalue("sad"): 
 	print("<p> Oh no! Why are you sad? </p>") 
+if form.getvalue('maths'):
+   math_flag = "ON"
+else:
+   math_flag = "OFF"
+
+if form.getvalue('physics'):
+   physics_flag = "ON"
+else:
+   physics_flag = "OFF"
+
+
 
 # Using HTML input and forms method 
 print("<form method='post' action='pythoncgi.py'>") 
 print("<p>Name: <input type='text' name='name' /></p>") 
 print("<input type='checkbox' name='happy' /> Happy") 
 print("<input type='checkbox' name='sad' /> Sad") 
-print("<input type='submit' value='Submit' />") 
-print("</form")
-print("</body></html>") 
+ 
+print("<br><h1>Checkbox - Third CGI Program</h1>")
+print("<h2> CheckBox Maths is : %s </h2> "% math_flag)
+print("<h2> CheckBox Physics is : %s </h2>" % physics_flag)
+print("<input type = 'radio' name = 'subject' value = 'maths' /> Maths")
+print("<input type = 'radio' name = 'subject' value = 'physics' /> Physics")
+print("<input type = 'submit' value = 'Select Subject' />")
+print("</form>")
+######################################################################
+
+
+
+#https://www.geeksforgeeks.org/cgi-programming-python/
+
+
+
+
 
---
+++ temp.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3 
+# Importing the 'cgi' module 
+
+import cgi
+from time import localtime 
+
+# function for create table and fill informations
+
+def HTMLtable(templist):
+    table = "<table style='width:50%'>" #config width of table
+    table += "<tr><th>DAY</th><th>TIME</th><th>temp(C)</th></tr>"   # write head of table
+    dicttemp = {}
+    for line in templist:
+        data = line.split()
+        daytime = data[0]+" "+data[1]
+        temp = data[2]
+        dicttemp[daytime] = float(temp)
+
+    sortTemp = sorted(dicttemp.values())
+    sortTemp.reverse()
+
+    for i in sortTemp:
+        datas = get_key(i,dicttemp).split()
+        day = datas[0]
+        time = datas[1]
+        table = table+"\n<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(day,time,str(i)) # fill information in table
+        
+    table += "</table>"
+    print(table)
+
+def get_key(val,dic):
+    for key, value in dic.items():
+        if val == value:
+            return key
+# This block setting style of table and show Hello Program!
+print("Content-type: text/html\r\n\r\n") 
+print("<html>")
+print("<head>")
+print("<style>")
+print("table,th,td { border: 1px solid black; }")
+print("</style>")
+print("</head>")
+print("<body>") 
+print("<h1> Hello Program! </h1>") 
+
+# Using the inbuilt method# Using the inbuilt methodsrw
+form = cgi.FieldStorage() 
+time = localtime()
+if form.getvalue("name"):
+    day = str(time.tm_mday) + "/" + str(time.tm_mon) + "/" + str(time.tm_year)  
+    time = str(time.tm_hour) +":" + str(time.tm_min) + ":" + str(time.tm_sec)
+    name = form.getvalue("name") 
+
+# This block write file that contain day/month/year hour/miniute/second temperature
+    f = open("demofile2.txt", "a")
+    f.write(day+" "+time+" "+name+ "\n")
+    f.close()
+
+# This block show information on browser
+f = open("demofile2.txt", "r")
+data_temp = f.read()
+print("<pre>"+data_temp+"</pre><br>") 
+with open("demofile2.txt") as file_in:
+    lines = []
+    for line in file_in:
+        lines.append(line)
+HTMLtable(lines)
+f.close()
+
+# create form that user can fill temperature
+print("<form method='post' action='temp.py'>") 
+print("<p>NUM: <input type='text' name='name'></p>") 
+print("<input type='submit' value='Submit'>") 
+print("</form>")
+print("</body></html>") 
