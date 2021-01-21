ecfedc9ba1d39b324a89533d31698706f8e41fac
Kantinun Sirintharawet <69186677+Kantinun@users.noreply.github.com>
2021-01-21 16:16:12 +0700

    Add stopwatch

--- factorial.py
+++ factorial.py
@@ -5,6 +5,14 @@ import time
 print("Content-type: text/html\r\n\r\n") 
 print("<html>")
 form = cgi.FieldStorage()
+
+def time_convert(sec):
+    mins = sec // 60
+    sec = sec % 60
+    hours = mins // 60
+    mins = mins % 60
+    print("<p>Time Lapsed = {0}:{1}:{2}</p>".format(int(hours),int(mins),sec))
+
 def factorial(num):
     result = 1
     for i in range(2,num+1):
@@ -17,7 +25,7 @@ if form.getvalue('name'):
     answer = factorial(int(num))
     end = time.time()
     print("<p> Result : {} </p>".format(answer))
-    print("<p> Time : {} </p>".format(end - start))
+    time_convert(end-start)
 
 print("<form method='post' action='factorial.py'>") 
 print("<p>NUM: <input type='text' name='name'></p>")
